c4e48e5bd726628c03158714b43156b60e70c963
David Scott <115048859+DScott2018@users.noreply.github.com>
2023-02-24 13:51:51 -0500

    Create getHealth.py

---
+++ user/getHealth.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+def gethealth():
+    username = "team2"
+    password = "team2now"
+
+    response = requests.get('https://jtektdev.ava8.net/api/htproxy/WebDrive/BA00140/robot/health',
+                            auth=(username, password))
+
+    if response.status_code == 200:
+        data = response.json()
+        out = json.dumps(data, indent=4)
+    else:
+        out = ('Failed with code: ', response.status_code)
+
+    return out
+
+
+print(gethealth())
